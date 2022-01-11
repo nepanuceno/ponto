f3e8b79a87d7cfb20b3b0758af9ac57cac667c41
Paulo Roberto Torres <paulo.torres.apps@gmail.com>
2022-01-11 20:03:24 -0300

    Botoes de gerenciamento de Usuarios

--- resources/views/positions/index.blade.php
+++ resources/views/positions/index.blade.php
@@ -67,7 +67,7 @@
             {{ $positions->links() }}
         </div>
     @else
-        <div class="alert alert-primary d-flex align-items-center" role="alert">
+        <div class="alert alert-info d-flex align-items-center" role="alert">
             <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:">
                 <use xlink:href="#info-fill" />
             </svg>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -34,38 +34,54 @@
             </div>
         </div>
     @endif
-
-    <table class="table table-bordered">
-        <tr>
-            <th>No</th>
-            <th>Nome</th>
-            <th>Email</th>
-            <th>Perfis</th>
-            <th width="280px">Ações</th>
-        </tr>
-
-        @foreach ($data as $key => $user)
+    <div class="table-responsive-sm table-responsive-md">
+        <table class="table table-bordered">
             <tr>
-                <td>{{ ++$i }}</td>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td>
-                    @if (!empty($user->getRoleNames()))
-                        @foreach ($user->getRoleNames() as $v)
-                            <label class="badge badge-success">{{ $v }}</label>
-                        @endforeach
-                    @endif
-                </td>
-                <td>
-                    <a class="btn btn-info" href="{{ route('users.show', $user->id) }}">Detalhes</a>
-                    <a class="btn btn-primary" href="{{ route('users.edit', $user->id) }}">Editar</a>
-                    {!! Form::open(['method' => 'DELETE', 'route' => ['users.destroy', $user->id], 'style' => 'display:inline']) !!}
-                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                    {!! Form::close() !!}
-                </td>
+                <th>No</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Perfis</th>
+                <th width="280px">Ações</th>
             </tr>
-        @endforeach
-    </table>
+
+            @foreach ($data as $key => $user)
+                <tr>
+                    <td>{{ ++$i }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>
+                        @if (!empty($user->getRoleNames()))
+                            @foreach ($user->getRoleNames() as $v)
+                                <label class="badge badge-success">{{ $v }}</label>
+                            @endforeach
+                        @endif
+                    </td>
+                    <td>
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-default">Ações</button>
+                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"
+                                aria-expanded="false">
+                                <span class="sr-only">Toggle Dropdown</span>
+                            </button>
+                            <div class="dropdown-menu" role="menu" style="">
+                                <a class="dropdown-item" href="{{ route('users.show', $user->id) }}">
+                                    <i class="fas fa-info mr-2"></i>Detalhes</a>
+                                <a class="dropdown-item" href="{{ route('users.edit', $user->id) }}">
+                                    <i class="fas fa-edit mr-2"></i>Editar</a>
+                                <div class="dropdown-divider"></div>
+                                {!! Form::open(['method' => 'DELETE', 'route' => ['users.destroy', $user->id], 'style' => 'display:inline']) !!}
+                                {{ Form::button('<i class="fas fa-trash mr-2"></i>Excluir', ['type' => 'submit', 'class' => 'dropdown-item']) }}
+                                {!! Form::close() !!}
+                            </div>
+                        </div>
+
+
+
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 
     {!! $data->render() !!}
 @endsection
